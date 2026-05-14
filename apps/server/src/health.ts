c4e0200a30f3633a2a8ae045898c5fbfc4ff446e
has-bii <hasbii.rofficial@gmail.com>
2026-05-14 10:11:15 +0700

    feat: add database package and add database on health api

--- apps/server/src/health.ts
+++ apps/server/src/health.ts
@@ -1,9 +1,19 @@
+import { prisma } from '@kashin/database'
 import { Hono } from 'hono'
 
 const health = new Hono()
 
 health.get('/', (c) => {
   return c.json({ status: 'up' })
+})
+
+health.get('/database', async (c) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`
+    return c.json({ status: 'up' })
+  } catch (e) {
+    return c.json({ status: 'down' }, 500)
+  }
 })
 
 export default health
